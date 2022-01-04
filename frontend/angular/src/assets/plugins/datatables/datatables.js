81fb56777f7784f0d5cdb1a9f7c1f8c89097a452
zachkirkrey <zachkirkrey@gmail.com>
2022-01-04 18:40:44 +0530

    changes

--- frontend/angular/src/assets/plugins/datatables/datatables.js
+++ frontend/angular/src/assets/plugins/datatables/datatables.js
@@ -15394,7 +15394,7 @@ var DataTable = $.fn.dataTable;
 /* Set the defaults for DataTables initialisation */
 $.extend( true, DataTable.defaults, {
 	dom:
-		"<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+		"<'row'<'col-sm-12 col-md-4'l><'col-sm-12 col-md-4'f>>" +
 		"<'row'<'col-sm-12'tr>>" +
 		"<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
 	renderer: 'bootstrap'
